b2de720020c91ee15a5f30ffa5794388105b8d5c
Victor Elgersma <elgersmav@gmail.com>
2026-08-19 11:22:38 +0100

    remove svg favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
